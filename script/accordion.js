8842ee6950d4f1114d091e8e8d8d8e26a3e25825
eniolajava <gloriaadams112@gmail.com>
2024-06-23 02:25:46 +0100

    working on the front-end

---
+++ script/accordion.js
@@ -0,0 +1,8 @@
+const accordionHeaders = document.querySelectorAll('.accordion-header');
+
+accordionHeaders.forEach(header => {
+  header.addEventListener('click', () => {
+    const content = header.nextElementSibling;
+    content.classList.toggle('active');
+  });
+});
